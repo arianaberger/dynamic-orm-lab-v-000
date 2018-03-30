d525a7166fc4d11b6f1a9da01179369dcf09cc18
Ariana Berger <ariana.berger@gmail.com>
2018-03-30 18:13:47 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -65,7 +65,7 @@ class InteractiveRecord
 
   def self.find_by(attribute)
     column = attribute.keys.flatten[0].to_s
-    data = attribute.values.flatten[0].to_s
+    data = attribute.values.flatten[0]
     # binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE #{column} = #{data}"
     DB[:conn].execute(sql)
